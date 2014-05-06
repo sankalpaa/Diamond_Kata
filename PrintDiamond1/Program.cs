f6d730da1dee5106ede38de415ff395c6d0b8e13
sankalpa <sab@exilesoft.com>
2014-05-06 09:36:06 +0530

    Second try with single loop

--- PrintDiamond1/Program.cs
+++ PrintDiamond1/Program.cs
@@ -1,94 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FluentAssertions;
 using NUnit.Framework;
 //18.06 pm
 //22.12
 namespace PrintDiamond1
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var printDiamondList = PrintDiamond('Z');
-			foreach (var line in printDiamondList)
-			{
-				Console.WriteLine(line);
-			}
-			Console.ReadLine();
-		}
+    class Program
+    {
+        static void Main()
+        {
+            var printDiamondList = PrintDiamond('S');
+            foreach (var line in printDiamondList)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
 
-		[Test]
-		public void PrintDiamond_ShouldReturn_AA_When_A()
-		{
-			PrintDiamond('A').Should().BeEquivalentTo(new List<string> {"A","A"});
-		}
+        [Test]
+        public void PrintDiamondShouldReturnAWhenA()
+        {
+            PrintDiamond('A').Should().BeEquivalentTo(new List<string>{"A"});
+        }
 
-		[Test]
-		public void PrintDiamond_ShouldReturn_ABBA_When_B()
-		{
-			PrintDiamond('B').Should().BeEquivalentTo(new List<string> { " A", "B B", " A" });
-		}
+        [Test]
+        public void PrintDiamondShouldReturnABAWhenB()
+        {
+            PrintDiamond('B').Should().BeEquivalentTo(new List<string> { " A","B B"," A" });
+        }
 
-		[Test]
-		public void PrintDiamond_ShouldReturn_ABCCBA_When_C()
-		{
-			PrintDiamond('C').Should().BeEquivalentTo(new List<string> { "  A", " B B","C   C", " B B", "  A" });
-		}
+        [Test]
+        public void PrintDiamondShouldReturnABCAWhenC()
+        {
+            PrintDiamond('C').Should().BeEquivalentTo(new List<string> { "  A", " B B", "C   C", " B B", "  A" });
+        }
 
-		[Test]
-		public void PrintDiamond_ShouldReturn_ABCDDCBA_When_D()
-		{
-			PrintDiamond('D').Should().BeEquivalentTo(new List<string> { "   A", "  B B", " C   C","D     D", " C   C", "  B B", "   A" });
-		}
+        [Test]
+        public void PrintDiamondShouldReturnABCDAWhenD()
+        {
+            PrintDiamond('D').Should().BeEquivalentTo(new List<string> { "   A", "  B B", " C   C", "D     D", " C   C", "  B B", "   A" });
+        }
 
-		[Test]
-		public void PrintDiamond_ShouldReturn_ABCDEEDCBA_When_E()
-		{
-			PrintDiamond('E').Should().BeEquivalentTo(new List<string> { "    A", "   B B", "  C   C", " D     D", "E       E", " D     D", "  C   C", "   B B", "    A" });
-		}
+        [Test]
+        public void PrintDiamondShouldReturnABCDEAWhenE()
+        {
+            PrintDiamond('E').Should().BeEquivalentTo(new List<string> { "    A", "   B B", "  C   C", " D     D", "E       E", " D     D", "  C   C", "   B B", "    A" });
+        }
 
-		private static List<string> PrintDiamond(char letter)
-		{
-			var letterAscillValue = (int) letter;
-			int currentLetterAsci = 65;
-			var diamondValues = new List<string>();
-			int lineLength = (letterAscillValue-65)*2+1;
-			int prefixLength = (lineLength-1)/2;
+        private static List<string> PrintDiamond(char charcter)
+        {
+            var diamondArray = new List<string>();
+            char seedChar='A';
+            bool isReverse = false;
+            int prefixSpacers = charcter - 'A';
+            while (seedChar <= charcter && seedChar >= 'A')
+            {
+                if (seedChar=='A')
+                    diamondArray.Add(string.Format("{0}{1}",new string(' ',prefixSpacers),seedChar));
+                else
+                    diamondArray.Add(GetString(charcter, seedChar));
 
-				while (currentLetterAsci <= letterAscillValue)
-				{
-					if (currentLetterAsci == 65)
-						diamondValues.Add(string.Format("{0}{1}", new String(' ', (lineLength - 1)/2), 'A'));
-					else
-					{
-						diamondValues.Add(string.Format("{0}{1}{2}{1}", new String(' ', prefixLength),
-														(char)currentLetterAsci, new String(' ', lineLength - prefixLength*2-2)));
+                if (seedChar == charcter)
+                    isReverse = true;
 
-					}
-					prefixLength--;
-					currentLetterAsci++;
-				}
-			
-			prefixLength = 1;
-			currentLetterAsci = letterAscillValue;
-				while (currentLetterAsci >= 65)
-				{
-					if (currentLetterAsci == 65)
-						diamondValues.Add(string.Format("{0}{1}", new String(' ', (lineLength - 1) / 2), 'A'));
-					else if (currentLetterAsci != letterAscillValue)
-					{
-						diamondValues.Add(string.Format("{0}{1}{2}{1}", new String(' ', prefixLength),
-														(char)currentLetterAsci, new String(' ', lineLength - prefixLength * 2 - 2)));
-						prefixLength++;
-					}
+                if (isReverse)
+                    seedChar--;
+                else
+                    seedChar++;
+            }
+            return diamondArray;
+        }
 
-					currentLetterAsci--;
-				}
-			return diamondValues;
-		}
-	}
+        private static string GetString(char charcter, char seedChar)
+        {
+            return string.Format("{0}{1}{2}{1}", new string(' ', charcter-seedChar), seedChar, new string(' ', ((seedChar - 'A')*2)-1));
+        }
+    }
 }
